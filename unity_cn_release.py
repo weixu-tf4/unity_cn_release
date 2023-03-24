dd237a6db0d5aab957f1d6590624b2a096a49725
weixu <weixu@nibirutech.com>
2023-03-24 18:44:09 +0800

    add template file

--- unity_cn_release.py
+++ unity_cn_release.py
@@ -3,6 +3,8 @@ import urllib.request as request
 import json
 import time
 
+from string import Template
+
 # get http code
 def get_http_code(url, major_ver):
     search_str = 'window.__props__ ='
@@ -11,7 +13,7 @@ def get_http_code(url, major_ver):
     http_text = r.read().decode('utf-8')
     start = http_text.index(search_str)
     end = http_text.index(';', start + len_search_str)
-    print(f'start {start} end {end}')
+    #print(f'start {start} end {end}')
     data = http_text[start + len_search_str: end]
     # json
     json_data = json.loads(data)
@@ -21,19 +23,42 @@ def get_http_code(url, major_ver):
 
 # parse unity version json data
 def handle_ver_list(ver_list_data):
+    template_win=''
+    with open('template_win.txt', 'r') as fi:
+        template_win = Template(fi.read())
+    
+    template_mac_x86=''
+    with open('template_macos_x86.txt', 'r') as fi:
+        template_mac_x86 = Template(fi.read())
+    
+    template_mac_arm=''
+    with open('template_macos_arm.txt', 'r') as fi:
+        template_mac_arm = Template(fi.read())
+
     for ver in ver_list_data:
         date_str = time.strftime('%Y-%m-%d', time.localtime(int(ver['created'])))
+        data = {
+            'hash': ver['chineseHash'],
+            'version': ver['title']+ver['chinesePostfix'],
+        }
         print(f'version: {ver["title"]} hash: {ver["chineseHash"]} date: {date_str}')
-        print('\tMacOS: ')
+        print('  MacOS: ')
+        print(f'  DownloadAssistant: {ver["downloadMac"]["unityInstaller"]}')
+        # mac x86
         if ver['downloadMac']['unityEditorIntel'] :
-            print(f'\t\tIntel: {ver["downloadMac"]["unityEditorIntel"]}')
+            print(f'  Intel:')
+            print(template_mac_x86.substitute(data))
+        # mac arm
         if ver['downloadMac']['unityEditorAppleSilicon'] :
-            print(f'\t\tAppleSilicon: {ver["downloadMac"]["unityEditorAppleSilicon"]}')
-        print('\tWindows: ')
+            print(f'  AppleSilicon:')
+            print(template_mac_arm.substitute(data))
+        # win
+        print('  Windows: ')
         if ver['downloadWin']['unityInstaller'] :
-            print(f'\t\tDownloadAssistant: {ver["downloadWin"]["unityInstaller"]}')
+            print(f'  DownloadAssistant: {ver["downloadWin"]["unityInstaller"]}')
         if ver['downloadWin']['unityEditor64'] :
-            print(f'\t\tWindows: {ver["downloadWin"]["unityEditor64"]}')
+            print(f'  Windows:')
+            print(template_win.substitute(data))
 
 if __name__ == '__main__':
     print(len(sys.argv))
